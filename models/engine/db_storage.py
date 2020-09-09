081ee2f892653a9a9e458c34fa03b54517b4ac78
mauricioolarte <reneolarte@yahoo.com>
2020-09-09 14:44:27 -0500

    actualice dbstorage casting cls

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,6 +69,10 @@ class DBStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
+        if type(cls) == str:
+            cls = classes.get(cls)
+        if cls is None:
+            return None
         return self.__session.query(cls).filter(cls.id == id).first()
 
     def count(self, cls=None):
@@ -76,6 +80,8 @@ class DBStorage:
         Returns the number of objects in storage matching the given class name
         If no name is passed, returns the count of all objects in storage
         """
+        if type(cls) is str:
+            cls = classes.get(cls)
         if cls is None:
             return len(self.all())
         return len(self.all(cls))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,16 +53,21 @@ class FileStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
+        if type(cls) is str:
+            cls = classes.get(cls)
+        if cls is None:
+            return None
         for item in self.__objects.values():
             if item.__class__ == cls and item.id == id:
                 return item
-        return None
 
     def count(self, cls=None):
         """A method to count the number of objects in storage
         Returns the number of objects in storage matching the given class name
         If no name is passed, returns the count of all objects in storage
         """
+        if type(cls) is str:
+            cls = classes.get(cls)
         if cls is None:
             return len(self.all())
         return len(self.all(cls))
